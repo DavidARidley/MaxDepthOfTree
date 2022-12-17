9b8bae8d21c730f98446563b38a0df6b2c6003d5
DavidARidley <118642356+DavidARidley@users.noreply.github.com>
2022-12-17 10:33:00 -0500

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,1 +1,25 @@
-
+public class Solution {
+	public static void main(String[] args) {
+		//Create a sample tree
+    TreeNode root = new TreeNode(3);
+		root.left = new TreeNode(9);
+		root.right = new TreeNode(20);
+		root.right.left = new TreeNode(8);
+		root.right.right = new TreeNode(10);
+		System.out.println(findDepth(root));
+	}
+	public static int findDepth(TreeNode root) {
+		//Base Case(s)
+		if(root == null) {
+			return 0;
+		}
+		//Find Count of Each Branch
+		int leftCount = 1 + findDepth(root.left);
+		int rightCount = 1 +  findDepth(root.right);
+		//Return max
+		if(leftCount > rightCount) {
+			return leftCount;
+		}
+		else return rightCount;  
+    }
+}
